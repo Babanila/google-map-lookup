bb811d05dfbb907deb2a769b4f17ab1fea41e5d0
Babanila <babanila@yahoo.com>
2020-08-22 13:08:25 +0200

    chore(deps): modify webpack config settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,34 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+
 module.exports = {
-    watch: true,
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            }
+watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader'
+          }
         ]
-    }
-};
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html'
+    })
+  ]
+}
