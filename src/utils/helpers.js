c5ebee671245ebd2330f4cbd797f75535bcbcf87
Babanila <babanila@yahoo.com>
2020-08-23 19:33:39 +0200

    feat(): add helpers file with different helpers functions

---
+++ src/utils/helpers.js
@@ -0,0 +1,22 @@
+import axios from 'axios'
+
+export const databaseUrl = '../data/testdaten.txt'
+
+export function splitString(inputString, inputSymbol) {
+  return inputString.split(`${inputSymbol}`)
+}
+
+export function getAddress(parsedAddress) {
+  return parsedAddress.length > 3
+    ? splitString(parsedAddress[3], ' ')[0]
+    : splitString(parsedAddress[2], ' ')[0]
+}
+
+export async function simpleFetcher(url) {
+  try {
+    const retrievedData = await axios(url)
+    return retrievedData
+  } catch (error) {
+    return error.response
+  }
+}
